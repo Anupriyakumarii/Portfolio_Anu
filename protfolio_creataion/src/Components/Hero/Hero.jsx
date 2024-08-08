aca47ec42fc4ee97daa0034bc855340c197549df
AnupriyaKumarii <anuyadav20001@gmail.com>
2024-08-08 22:11:32 +0530

    hero

---
+++ protfolio_creataion/src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>Hey, I am Anupriya Kumari</h1>
+      <h1>Frontend Developer</h1>
+      <p>I am Frontend Developer with 2 years of Experience.</p>
+    </div>
+  );
+};
+
+export default Hero;
