309e98d521ee17788c6a4741380ba4d1acb25e7b
AnupriyaKumarii <anuyadav20001@gmail.com>
2024-08-10 22:57:19 +0530

    hero

--- protfolio_creataion/src/Components/Hero/Hero.jsx
+++ protfolio_creataion/src/Components/Hero/Hero.jsx
@@ -3,9 +3,11 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="hero">
-      <h1>Hey, I am Anupriya Kumari</h1>
-      <h1>Frontend Developer</h1>
-      <p>I am Frontend Developer with 2 years of Experience.</p>
+      <h1 className="intro">Hey, I am Anupriya Kumari</h1>
+      <h1 className="profession">Frontend Developer</h1>
+      <p className="about">
+        I am Frontend Developer with 2 years of Experience.
+      </p>
     </div>
   );
 };
--- protfolio_creataion/src/Components/Navbar/Navbar.jsx
+++ protfolio_creataion/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,15 @@
 import "./Navbar.css";
-import { MdOutlineNightlight } from "react-icons/md";
 
 const Navbar = () => {
   return (
     <>
       <div className="navbar">
-        {/* <img src={logo_img} alt="logo" className="logo"></img> */}
-        <h1 className="logo">Anu.</h1>
-
         <ul className="nav_menu">
           <li>Home</li>
           <li>About</li>
           <li>Experience</li>
           <li>Projects</li>
           <li>Contacts</li>
-          <MdOutlineNightlight />
         </ul>
       </div>
     </>
