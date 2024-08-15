ca6f39f7a0fbe969b0c514a73db8e7f4d77ef95a
AnupriyaKumarii <anuyadav20001@gmail.com>
2024-08-15 21:14:11 +0530

    navbar

--- protfolio_creataion/src/Components/Navbar/Navbar.jsx
+++ protfolio_creataion/src/Components/Navbar/Navbar.jsx
@@ -4,25 +4,23 @@ const Navbar = () => {
   return (
     <>
       <div className="navbar">
-        <nav className="nav">
-          <ul className="nav_menu">
-            <li className="nav-link">
-              <a href="Home">Home</a>
-            </li>
-            <li className="nav-link">
-              <a href="About">About</a>
-            </li>
-            <li className="nav-link">
-              <a href="Experience">Experience</a>
-            </li>
-            <li className="nav-link">
-              <a href="Project">Projects</a>
-            </li>
-            <li className="nav-link">
-              <a href="Contact">Contacts</a>
-            </li>
-          </ul>
-        </nav>
+        <ul className="nav_menu">
+          <li className="nav-link">
+            <a href="Home">Home</a>
+          </li>
+          <li className="nav-link">
+            <a href="About">About</a>
+          </li>
+          <li className="nav-link">
+            <a href="Experience">Experience</a>
+          </li>
+          <li className="nav-link">
+            <a href="Project">Projects</a>
+          </li>
+          <li className="nav-link">
+            <a href="Contact">Contacts</a>
+          </li>
+        </ul>
       </div>
     </>
   );
