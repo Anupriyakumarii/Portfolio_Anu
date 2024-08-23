48a8dcc2ce08ad91e0cdfa32a0f5c025ea426e28
Anupriya Kumari <anuyadav20001@gmail.com>
2024-08-24 00:17:55 +0530

    changes of about section

--- protfolio_creataion/src/Components/About/About.jsx
+++ protfolio_creataion/src/Components/About/About.jsx
@@ -7,14 +7,13 @@ function About() {
         <div className="glitch">
           <h1>ABOUT ME</h1>
         </div>
-        {/* <div className="glow">ABOUT</div> */}
         <div className="inner_card">
           <div className="image_scr">
             <img src={image} className="image_scr"></img>
           </div>
-          <div className="About">
-            <p></p>
-          </div>
+          <p className="About">
+              Over 2 years of experience as a Frontend Developer at Tata Consultancy Services (TCS), with a successful project deployment at Zebra Technologies which is a client location of TCS. Specialize in JavaScript, React, UI/UX design and CSS Framework.Demonstrated proficiency in developing high-performing, responsive user interfaces, optimizing code, and fostering effective collaboration in agile teams.
+          </p> 
         </div>
       </div>
     </>
