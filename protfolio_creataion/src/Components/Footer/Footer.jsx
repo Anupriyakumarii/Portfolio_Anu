acbf13f0ccac744b033368c5e304982d3200d5cf
Anupriya Kumari <anuyadav20001@gmail.com>
2024-12-30 22:04:11 +0530

    footer

--- protfolio_creataion/src/Components/Footer/Footer.jsx
+++ protfolio_creataion/src/Components/Footer/Footer.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
+    <>
     <div className="glitch">
       <h1 className='heading'>Projects</h1>
     </div>
+    </>
+    
   )
 }
 
