27ebfb95477d510bbea73096bf5b6e0faabe579f
Anupriya Kumari <anuyadav20001@gmail.com>
2025-01-04 20:12:59 +0530

    footer

--- protfolio_creataion/src/Components/Footer/Footer.jsx
+++ protfolio_creataion/src/Components/Footer/Footer.jsx
@@ -4,9 +4,9 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <>
-    <div className="glitch">
-      <h1 className='heading'>Footer</h1>
-    </div>
+    <footer>
+      
+    </footer>
     </>
     
   )
