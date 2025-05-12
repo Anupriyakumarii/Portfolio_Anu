c77b296eebc56b7785add26b19c8911684085bd5
Anupriya Kumari <anuyadav20001@gmail.com>
2025-05-12 10:19:31 +0530

    experience

--- protfolio_creataion/src/Components/Experience/Experience.jsx
+++ protfolio_creataion/src/Components/Experience/Experience.jsx
@@ -1,98 +1,105 @@
-import React from 'react';
+import React from "react";
 import "./Experience.css";
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBriefcase,
   faSchool,
   faStar,
-} from '@fortawesome/free-solid-svg-icons';
+} from "@fortawesome/free-solid-svg-icons";
 const workIcon = {
   icon: <FontAwesomeIcon icon={faBriefcase} />,
-  iconStyle: { background: 'rgb(33, 150, 243)', color: '#fff' },
+  iconStyle: { background: "rgb(33, 150, 243)", color: "#fff" },
 };
 const schoolIcon = {
   icon: <FontAwesomeIcon icon={faSchool} />,
-  iconStyle: { background: 'rgb(233, 30, 99)', color: '#fff' },
+  iconStyle: { background: "rgb(233, 30, 99)", color: "#fff" },
 };
 const starIcon = {
   icon: <FontAwesomeIcon icon={faStar} />,
-  iconStyle: { background: 'rgb(16, 204, 82)', color: '#fff' },
+  iconStyle: { background: "rgb(16, 204, 82)", color: "#fff" },
 };
 
 const Experience = () => {
   const timeline = [
     {
       icon: workIcon,
-      date: '2022 - present',
-      title: 'Frontend Developer',
-      subtitle: 'Tata Consultancy services, Bangalore',
-      desc: 'Developed responsive React UIs, optimized performance, and implemented Dark Mode for improved user experience. Collaborated with teams for efficient design and development processes, ensuring high-quality code and timely delivery.Expert in React,JavaScript,CSS Framework,RESTApi, React Native.',
+      date: "2023 - Present",
+      title: "Frontend Developer",
+      subtitle: "Tata Consultancy services, Bangalore",
+      desc: "I’m a Front-End Developer with over two years of experience crafting responsive, high-performance web applications using modern technologies. I specialize in JavaScript, React, and CSS frameworks like Tailwind and SASS, with a strong focus on clean, maintainable code and intuitive user interfaces. My expertise spans building scalable components, integrating RESTful APIs, and optimizing UI performance. I’m passionate about creating seamless user experiences, collaborating across teams, and continuously improving through new challenges and learning opportunities.",
     },
     {
       icon: workIcon,
-      date: 'Sept-Dec 2020',
-      title: 'Software Trainee',
-      subtitle: 'Zensar Technologies, Pune',
-      desc: 'While interning at Zensar, I contributed to a project that involved analyzing customer behavior data. Using Python, I developed a script to automate data extraction from our CRM system. I then employed SQL to clean and transform the data, preparing it for analysis. My work led to valuable insights that informed our marketing strategy.',
+      date: "2022 - 2023",
+      title: "Jr.Frontend Developer",
+      subtitle: "Tata Consultancy services, Bangalore",
+      desc: "Developed responsive React UIs, optimized performance, and implemented Dark Mode for improved user experience. Collaborated with teams for efficient design and development processes, ensuring high-quality code and timely delivery.Expert in React,JavaScript,CSS Framework,RESTApi, React Native.",
     },
-   
-    
+    {
+      icon: workIcon,
+      date: "Sept-Dec 2020",
+      title: "Software Trainee",
+      subtitle: "Zensar Technologies, Pune",
+      desc: "While interning at Zensar, I contributed to a project that involved analyzing customer behavior data. Using Python, I developed a script to automate data extraction from our CRM system. I then employed SQL to clean and transform the data, preparing it for analysis. My work led to valuable insights that informed our marketing strategy.",
+    },
+
     {
       icon: schoolIcon,
-      date: '2018-2022',
-      title: 'Bharati Vidyapeeth college of Engineering for Women,Pune',
-      subtitle: 'Electronics & Telecommunication',
-      desc: '3rd Rank Holder in my course.In addition to my undergraduate degree, I pursued an honors program in data science. This specialized program deepened my understanding of data analysis and equipped me with the practical skills to extract valuable insights from complex datasets.',
+      date: "2018-2022",
+      title: "Bharati Vidyapeeth college of Engineering for Women,Pune",
+      subtitle: "Electronics & Telecommunication",
+      desc: "3rd Rank Holder in my course.In addition to my undergraduate degree, I pursued an honors program in data science. This specialized program deepened my understanding of data analysis and equipped me with the practical skills to extract valuable insights from complex datasets.",
     },
-   
+
     { icon: starIcon },
   ];
 
   return (
-    
     <div className="App">
       <div className="glitch">
-         <h1 className='heading'>Experience</h1>
-    </div>
+        <h1 className="heading">Experience</h1>
+      </div>
       <VerticalTimeline>
         {timeline.map((t, i) => {
           const contentStyle =
             i === 0
-              ? { background: 'rgb(33, 150, 243)', color: '#fff' }
+              ? { background: "rgb(33, 150, 243)", color: "#fff" }
               : undefined;
           const arrowStyle =
             i === 0
-              ? { borderRight: '7px solid  rgb(33, 150, 243)' }
+              ? { borderRight: "7px solid  rgb(33, 150, 243)" }
               : undefined;
-              return (
-                <VerticalTimelineElement
-                  key={i}
-                  className="vertical-timeline-element--work"
-                  contentStyle={contentStyle}
-                  contentArrowStyle={arrowStyle}
-                  date={t.date}
-                  {...t.icon}
-                >
-                  {t.title ? (
-                    <React.Fragment>
-                      <h3 className="vertical-timeline-element-title">{t.title}</h3>
-                      {t.subtitle && (
-                        <h4 className="vertical-timeline-element-subtitle">
-                          {t.subtitle}
-                        </h4>
-                      )}
-                      {t.desc && <p>{t.desc}</p>}
-                    </React.Fragment>
-                  ) : undefined}
-                </VerticalTimelineElement>
-              );
-            })}
-          </VerticalTimeline>
-        </div>
-      );
-  
-}
+          return (
+            <VerticalTimelineElement
+              key={i}
+              className="vertical-timeline-element--work"
+              contentStyle={contentStyle}
+              contentArrowStyle={arrowStyle}
+              date={t.date}
+              {...t.icon}
+            >
+              {t.title ? (
+                <React.Fragment>
+                  <h3 className="vertical-timeline-element-title">{t.title}</h3>
+                  {t.subtitle && (
+                    <h4 className="vertical-timeline-element-subtitle">
+                      {t.subtitle}
+                    </h4>
+                  )}
+                  {t.desc && <p>{t.desc}</p>}
+                </React.Fragment>
+              ) : undefined}
+            </VerticalTimelineElement>
+          );
+        })}
+      </VerticalTimeline>
+    </div>
+  );
+};
 
 export default Experience;
